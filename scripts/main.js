68604411a50fddaf92e2e6707401a3ca34e5e77b
josuerx12 <josuerx12@gmail.com>
2023-04-27 16:52:45 -0300

    12

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,43 @@
-//variables
-const url = `https://economia.awesomeapi.com.br/json/last/-`
-const currency = document.getElementById('moeda-primaria')
+// // Axios
 
-//events
-const currencyValue = currency.addEventListener('onChange', (e) => (e.target.value))
+const api = axios.create({
+    baseURL: 'https://economia.awesomeapi.com.br/json/last/',
+})
 
-console.log(currencyValue)
+api.get(`${firstOptionValue}-${secondOptionValue}`)
+    .then((response) => {
+        const infos = response.data
+        console.log (infos.USDBRL.ask)
+    })
+    .catch(error => window.alert('Carregamento ocorreu de forma incorreta.'))
+
+//
+
+const moedas = document.querySelectorAll('select')
+
+function moedaPrimaria(firstOption){
+    moedas[0].addEventListener('change', () => {
+        const moeda = moedas[0].value
+        firstOption = moeda
+    })
+    return firstOption
+}
+console.log(moedaSecundaria(firstOption))
+
+function moedaSecundaria(secondOption){
+    moedas[1].addEventListener('change', () => {
+        const moeda = moedas[1].value
+        secondOption = moeda
+    })
+    return secondOption
+}
+console.log(moedaSecundaria(secondOption))
+
+function calcular() {
+    const input = document.getElementById('valoraserconvertido')
+    input.addEventListener('input', () => {
+        let inputValue = input.value.replace(',', '.')
+        console.log(inputValue)
+    })
+}
+calcular()
