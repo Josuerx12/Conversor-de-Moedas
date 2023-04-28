fbf533ac64a0d95ee861732e900f2298c731a8c2
josuerx12 <josuerx12@gmail.com>
2023-04-28 14:04:39 -0300

    finally with bugs

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ const urlMoedas = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata
 //variaveis globais do documento
 const moedasSelect = document.querySelectorAll('select')
 const btn = document.querySelector('.btn')
+const display = document.getElementById('resultado')
 
 async function listDeMoedas() {
     await axios.get(urlMoedas)
@@ -30,16 +31,26 @@ listDeMoedas()
 btn.addEventListener('click', function calculator() {
     const moedaOrigem = document.getElementById('moeda1')
     const moedaDestino = document.getElementById('moeda2')
+    const input = document.getElementById('valoraserconvertido')
     //axios create
     let moedaOrigemValue = moedaOrigem.value
     let moedaDestinoValue = moedaDestino.value
+    let inputValue = input.value.replace(',','.')
+
+    console.log(inputValue)
+
     const api = axios.create ({
-        baseURL: 'https://api.apilayer.com/exchangerates_data/live?',
-        Headers: {'apikey': 'ukcHwJLTgc9KXOTt77cGXWMTTeDUHp2G'},
+        baseURL: 'https://api.apilayer.com/exchangerates_data/',
+        headers: {'apikey': 'ukcHwJLTgc9KXOTt77cGXWMTTeDUHp2G'},
     })
         async function ValuesCurrency() {
-            await api.get(`convert?to=${moedaOrigemValue}&from=${moedaDestinoValue}&amount=${moedaDestinoValue}`)
-            .then((response) => console.log(response))
+            await api.get(`convert?to=${moedaDestinoValue}&from=${moedaOrigemValue}&amount=${inputValue}`)
+            .then((response) => {
+                const resultadoAPI = response.data.result
+                const resultado = resultadoAPI.toFixed(2)
+                console.log(resultado)
+                display.value = resultado
+            })
             .catch(error => console.error(`Falhou carregamento dos valores das moedas`))
         }
     ValuesCurrency()
